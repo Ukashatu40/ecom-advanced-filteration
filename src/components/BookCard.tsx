936e639789e88e88ecf2743afe53a088dcbb4dff
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-09-10 12:01:26 +0100

    feat/ added boxshadow and removed border

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -9,7 +9,7 @@ interface BookCardProps {
 
 const BookCard: React.FC<BookCardProps> = ({ id, title, image, price }) => {
   return (
-    <div className="border p-4 rounded-lg" >
+    <div className="p-4 rounded-lg" style={{boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
       <Link to={`/product/${id}`}>
         <img
           src={image}
